0f32fcbc696d85d3a049c5a5d7f41776c5c3d0cf
microbizmedia <info@microbizmedia.com>
2025-08-08 13:33:31 -0600

    Working on header.

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,80 @@
+'use client'
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Navbar from '@/app/components/Nav/Nav';
+
+const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  // useEffect(() => {
+  //   const handleHashChange = () => {
+  //     const element = document.getElementById(window.location.hash.substring(1));
+  //     if (element) {
+  //       element.classList.remove("jos-active"); // Remove animation class
+  //       setTimeout(() => {
+  //         element.classList.add("jos-active"); // Re-add animation class to trigger it
+  //       }, 50);
+  //     }
+  //   };
+
+  //   window.addEventListener("hashchange", handleHashChange);
+
+  //   return () => {
+  //     window.removeEventListener("hashchange", handleHashChange);
+  //   };
+  // }, []);
+
+  return (
+    <header className='site-header z-50' id='sticky-menu' >
+      <div className=' max-w-full m-auto lg:pl-[36px]  '>
+        <div className='flex items-center justify-between h-[65px]'>
+
+
+
+
+          {/* Header Logo */}
+          <Link href='/web/'>
+            <img
+              className='hidden lg:block'
+              src='/logo_standard.png'
+              alt='Logo desktop'
+              width='135'
+              height='24'
+            />
+            <img
+              className='block lg:hidden'
+              src='logo_mobile.png'
+              alt='Logo mobile'
+            />
+          </Link>
+
+          <Navbar
+            mobileMenu={mobileMenu}
+            setMobileMenu={setMobileMenu}
+          />
+          {/* price button */}
+          
+          <div className='flex items-center '>
+            <Link href="/web/services#target-pricing" className=' py-[20.5px] px-[36px] ml-[50px] border-none 
+             bg-[#746eb8] after:bg-violet-600  lg:inline-block'>
+              Pricing
+            </Link>
+
+
+            {/* Responsive Off-canvas Menu Button */}
+            <div className='block lg:hidden'>
+              <button onClick={() => setMobileMenu(true)} className={`mobile-menu-trigger `} >
+                <span />
+              </button>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/components/Nav/Nav.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import './menu.css'
+// import threadsIcon from '../footer/social_media_icons/threads_icon.png'
+// import xIcon from '../footer/social_media_icons/x_icon_b.png'
+// import instagramIcon from '../footer/social_media_icons/instagram_icon_b.png'
+
+
+// eslint-disable-next-line react/prop-types
+const Navbar = ({ mobileMenu, setMobileMenu }: any) => {
+    const [mobileSubMenu, setMobileSubMenu] = useState('');
+
+    const handleMenu = () => {
+        setMobileMenu(false);
+        setMobileSubMenu('');
+    };
+
+    return (
+        <div className='menu-block-wrapper'>
+
+            <div onClick={handleMenu} className={`menu-overlay ${mobileMenu && 'active'}`} />
+
+            <nav className={`menu-block ${mobileMenu && 'active'}`} id='append-menu-header'  >
+
+                <ul className={`site-menu-main`}>
+                    <li onClick={handleMenu} className='nav-item' >
+                        <Link href='' className='nav-link-item '>   Our Work  </Link>
+                    </li>
+                    <li onClick={handleMenu} className='nav-item'>
+                        <Link href='' className='nav-link-item '>  Services  </Link>
+                    </li>
+                    <li onClick={handleMenu} className='nav-item' >
+                        <Link href='' className='nav-link-item'>    Process </Link>
+                    </li>
+                    <li onClick={handleMenu} className='nav-item ' >
+                        <Link href='' className='nav-link-item '>     Careers  </Link>
+                    </li>
+                    <li className='nav-item' onClick={handleMenu}   >
+                        <Link href='' className='nav-link-item '>   About Us </Link>
+                    </li>
+                </ul>
+
+                {/* <div className='dropdown-menu-social-media-icons'>
+          <img
+            src={threadsIcon}
+            alt='threads-icon'
+            className='  size-10'
+          />
+          <img
+            src={xIcon}
+            alt='x-icon'
+            className='  size-10'
+          />
+          <img
+            src={instagramIcon}
+            alt='instagram-icon'
+            className='  size-10'
+          />
+        </div> */}
+                <div className={`mobile-menu-head ${mobileSubMenu && 'active'}`}>
+                    <div onClick={handleMenu} className='mobile-menu-close rotate-[-90deg]'>
+                        &#10095;
+                    </div>
+                </div>
+
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
