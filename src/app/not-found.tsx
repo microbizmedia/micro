4af818147d235c3dca11083923d0cb8e427c2008
microbizmedia <info@microbizmedia.com>
2025-08-11 03:22:20 -0600

    ...slug page.tsx added to control not found reload.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-const Error_404 = () => {
+export default function NotFound() {
   return (
     <main id='404-section' className='global-container flex flex-col items-center justify-center  text-center mt-28 mb-20 '>
-      <Image src='/404_error_image.png' loading="lazy" alt='404 error image'  width={400} height={400}/>
+      <Image src='/404_error_image.png' priority alt='404 error image'  width={400} height={400}/>
       <h1 className='pt-8'> Oops! Page not found </h1>
       <p className='sub-title jos tracking-wider slide-from-bottom m-auto py-6'>  Sorry for the inconvenience, let&apos;s go home.  </p>
       <Link href='/' className='button rounded-md' aria-label="Go back to homepage"> Take me home &#x27a4;  </Link>
@@ -12,4 +12,3 @@ const Error_404 = () => {
   );
 };
 
-export default Error_404;
