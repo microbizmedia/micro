aec183c55ca3b3090a6f062a315bde051d29c596
microbizmedia <info@microbizmedia.com>
2025-08-14 12:30:49 -0600

    Improving website performance...

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function NotFound() {
   return (
-    <main id='404-section' className='global-container flex flex-col items-center justify-center  text-center mt-28 mb-20 h-[50vh]' data-aos='zoom-in'>
+    <main id='404-section' className='global-container flex flex-col items-center justify-center  text-center mt-28 mb-20'>
       <Image src='/404_error_image.png' loading='lazy' alt='404 error image'  width={400} height={400}/>
       <h1 className='pt-8'> Oops! Page not found </h1>
       <p className='sub-title m-auto py-6'>  Sorry for the inconvenience, let&apos;s go home.  </p>
--- src/app/types/jos-animation.d.ts
+++
@@ -1,2 +0,0 @@
-declare module 'jos-animation/dist/jos.js';
-declare module 'jos-animation/dist/jos.css';
