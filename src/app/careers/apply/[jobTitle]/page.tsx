192ea59376eb55edb443cc6bcf0859a30d470d61
microbizmedia <info@microbizmedia.com>
2025-08-15 14:25:02 -0600

    Working and testing on carrers applications.

--- src/app/careers/apply/[jobTitle]/page.tsx
+++ src/app/careers/apply/[jobTitle]/page.tsx
@@ -88,7 +88,7 @@ const CareersAplications = () => {
 
 
         try {
-            const response = await fetch("http://localhost:5000/apply", {
+            const response = await fetch("https://micro-biz-backend.vercel.app/apply", {
                 method: "POST",
                 body: applicationData,
             });
